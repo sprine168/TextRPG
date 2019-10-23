16151a481e1d9e84b565a9522d554b1c7eb4eb7f
steven <sprine168>
2019-10-23 17:27:29 -0500

    Added a method test to test the program, and it completes the test.

--- test/Tests.java
+++ test/Tests.java
@@ -1,7 +1,7 @@
 import org.junit.Test;
+import textrpg.Player;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 
 public class Tests {
@@ -12,8 +12,15 @@ public class Tests {
     }
 
     @Test
-    public void testFalse(){
+    public void testFalse() {
         assertFalse(true);
+    }
+
+    @Test
+    public void testItems() {
+
+        Player players = new Player("steven ", 1);
+        assertEquals(1, players.getName());
     }
 
 }
