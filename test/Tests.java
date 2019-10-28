d22cc58ab0444c5ea82ea2091328580394fa56bf
steven <sprine168>
2019-10-28 11:17:01 -0500

    Added a test for Item, it's just for showing proof of concept than an actual good test.

--- test/Tests.java
+++ test/Tests.java
@@ -2,9 +2,9 @@ import org.junit.Assert;
 import org.junit.Test;
 import textrpg.Player;
 import textrpg.items.Item;
+import textrpg.shops.Shop;
 
 import static org.junit.Assert.*;
-
 
 public class Tests {
 
@@ -20,6 +20,9 @@ public class Tests {
 
 
     // Testing Take Item
+
+    @Test
+    public void testStorage(){ }
 
     @Test
     public void testItems() {
