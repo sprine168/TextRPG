e1aab5aa26541632e65d096a217494eef2e2a3ae
kslater3 <kslater3@missouriwestern.edu>
2019-11-18 11:38:23 -0600

    Test For Equipment

--- src/test/EquiptmentTest.java
+++ src/test/EquiptmentTest.java
@@ -4,14 +4,30 @@
  * */
 
 
+import org.junit.Assert;
 import org.junit.Test;
+import textrpg.equipment.Equipment;
+
 import static org.junit.Assert.*;
 
 public class EquiptmentTest {
 
+    Equipment dummy_equipment;
+
+
     @Test
     public void testEquiptment(){
+        dummy_equipment = new Equipment();
 
+        dummy_equipment.setEquipmentType(1);
+
+        int[] stats = {1, 2, 3};
+        dummy_equipment.setEquipmentStats(stats);
+
+
+        Assert.assertEquals(1, dummy_equipment.getEquipmentType());
+
+        Assert.assertArrayEquals(stats, dummy_equipment.getEquipmentStats());
     }
 
 }
